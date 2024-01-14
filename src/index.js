05c9d2af2136569578b6dd4376c4c8ab3ffd9798
Tenadopt <yury.ignatiev@gmail.com>
2024-01-14 18:27:21 +0300

    add calculator logic plus dark css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,198 @@
-console.log("ffff");
+import "./styles/dark.css";
+
+// DOM Elements
+const valueEl = document.querySelector(".value");
+
+const acEl = document.querySelector(".ac");
+const pmEl = document.querySelector(".pm");
+const percentEl = document.querySelector(".percent");
+
+const additionEl = document.querySelector(".addition");
+const subtractionEl = document.querySelector(".subtraction");
+const multiplicationEl = document.querySelector(".multiplication");
+const divisionEl = document.querySelector(".division");
+const equalEl = document.querySelector(".equal");
+
+const decimalEl = document.querySelector(".decimal");
+const number0El = document.querySelector(".number-0");
+const number1El = document.querySelector(".number-1");
+const number2El = document.querySelector(".number-2");
+const number3El = document.querySelector(".number-3");
+const number4El = document.querySelector(".number-4");
+const number5El = document.querySelector(".number-5");
+const number6El = document.querySelector(".number-6");
+const number7El = document.querySelector(".number-7");
+const number8El = document.querySelector(".number-8");
+const number9El = document.querySelector(".number-9");
+const numberElArray = [
+  number0El,
+  number1El,
+  number2El,
+  number3El,
+  number4El,
+  number5El,
+  number6El,
+  number7El,
+  number8El,
+  number9El,
+];
+
+let valueStrInMemory = null;
+let operatorInMemory = null;
+let shouldClearDisplay = false;
+let hasResult = false;
+
+// Get the current displayed value as a string
+const getValueAsStr = () => valueEl.textContent.split(",").join("");
+
+// Get the current displayed value as a number
+const getValueAsNum = () => {
+  return parseFloat(getValueAsStr());
+};
+
+// Set a string as the displayed value with proper formatting
+const setStrAsValue = (valueStr) => {
+  if (valueStr === "Infinity") {
+    valueEl.textContent = "Err";
+    return;
+  }
+  if (valueStr[valueStr.length - 1] === ".") {
+    valueEl.textContent += ".";
+    return;
+  }
+
+  const [wholeNumStr, decimalStr] = valueStr.split(".");
+  let formattedValue;
+
+  if (decimalStr) {
+    const trimmedDecimalStr = parseFloat("0." + decimalStr)
+      .toString()
+      .slice(2);
+    formattedValue = parseFloat(
+      wholeNumStr + "." + trimmedDecimalStr,
+    ).toLocaleString();
+  } else {
+    formattedValue = parseFloat(wholeNumStr).toLocaleString();
+  }
+
+  valueEl.textContent = formattedValue;
+};
+
+// Handle click events for number buttons
+const handleNumberClick = (numStr) => {
+  if (hasResult) {
+    setStrAsValue(numStr);
+    hasResult = false;
+  } else if (shouldClearDisplay) {
+    setStrAsValue(numStr);
+    shouldClearDisplay = false;
+  } else {
+    const currentValueStr = getValueAsStr();
+    setStrAsValue(currentValueStr + numStr);
+  }
+};
+
+// Calculate the result of the operation and return as a string
+const getResultOfOperationAsStr = () => {
+  const currentValueNum = getValueAsNum();
+  const valueNumInMemory = parseFloat(valueStrInMemory);
+  let newValueNum;
+  if (operatorInMemory === "addition") {
+    newValueNum = valueNumInMemory + currentValueNum;
+  } else if (operatorInMemory === "subtraction") {
+    newValueNum = valueNumInMemory - currentValueNum;
+  } else if (operatorInMemory === "multiplication") {
+    newValueNum = valueNumInMemory * currentValueNum;
+  } else if (operatorInMemory === "division") {
+    newValueNum = valueNumInMemory / currentValueNum;
+  }
+  return newValueNum.toString();
+};
+
+// Handle click events for operator buttons
+const handleOperatorClick = (operation) => {
+  const currentValueStr = getValueAsStr();
+
+  if (!valueStrInMemory) {
+    valueStrInMemory = currentValueStr;
+    operatorInMemory = operation;
+    shouldClearDisplay = true;
+  } else {
+    valueStrInMemory = getResultOfOperationAsStr();
+    operatorInMemory = operation;
+    shouldClearDisplay = true;
+  }
+
+  hasResult = false;
+  updateDisplay();
+};
+
+// Update the displayed value
+const updateDisplay = () => {
+  const currentValueStr = getValueAsStr();
+  setStrAsValue(currentValueStr);
+};
+
+// Add Event Listeners to buttons
+acEl.addEventListener("click", () => {
+  setStrAsValue("0");
+  valueStrInMemory = null;
+  operatorInMemory = null;
+});
+pmEl.addEventListener("click", () => {
+  const currentValueNum = getValueAsNum();
+  const currentValueStr = getValueAsStr();
+
+  if (currentValueStr === "-0") {
+    setStrAsValue("0");
+    return;
+  }
+  if (currentValueNum >= 0) {
+    setStrAsValue("-" + currentValueStr);
+  } else {
+    setStrAsValue(currentValueStr.substring(1));
+  }
+});
+percentEl.addEventListener("click", () => {
+  const currentValueNum = getValueAsNum();
+  const newValueNum = currentValueNum / 100;
+  setStrAsValue(newValueNum.toString());
+  valueStrInMemory = null;
+  operatorInMemory = null;
+});
+
+// Add event listeners to operator buttons
+additionEl.addEventListener("click", () => {
+  handleOperatorClick("addition");
+});
+subtractionEl.addEventListener("click", () => {
+  handleOperatorClick("subtraction");
+});
+multiplicationEl.addEventListener("click", () => {
+  handleOperatorClick("multiplication");
+});
+divisionEl.addEventListener("click", () => {
+  handleOperatorClick("division");
+});
+equalEl.addEventListener("click", () => {
+  if (valueStrInMemory) {
+    setStrAsValue(getResultOfOperationAsStr());
+    valueStrInMemory = null;
+    operatorInMemory = "equal";
+    hasResult = true;
+  }
+});
+
+// Add Event Listeners to number and decimal buttons
+for (let i = 0; i < numberElArray.length; i++) {
+  const numberEl = numberElArray[i];
+  numberEl.addEventListener("click", () => {
+    handleNumberClick(i.toString());
+  });
+}
+decimalEl.addEventListener("click", () => {
+  const currentValueStr = getValueAsStr();
+  if (!currentValueStr.includes(".")) {
+    setStrAsValue(currentValueStr + ".");
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,29 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, "src", "index.js"),
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "[name].[contenthash].js",
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, "public", "index.html"),
-                favicon: path.resolve(__dirname, "public", "favicon.ico")
-            }
-        )
+  mode: "production",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: path.resolve(__dirname, "public", "favicon.ico"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-leader", "css-loader"]
-            }
-        ]
-
-    },
-    devServer: {
-        port: 3000,
-        open: true
-    }
-}
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+};
