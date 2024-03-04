c7e79318ff542861e48b1ea69b0197ea62aed5ba
Tenadopt <yury.ignatiev@gmail.com>
2024-03-04 14:00:58 +0300

    feat: add percent

---
+++ src/commands/PercentCommand.js
@@ -0,0 +1,14 @@
+export class PercentCommand {
+    constructor(value) {
+        this.value = value;
+    }
+
+    execute() {
+        this.result = this.value / 100;
+        return this.result;
+    }
+
+    redo() {
+        return this.result * 100;
+    }
+}
